cd4f22c16996796fd0d827b91a1c275d9f042093
Georgii Voyakin <voyaking@gmail.com>
2022-05-30 17:54:29 +0300

    Add checking if note exists

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -4,14 +4,17 @@ class noteController {
   async create(req, res) {
     try {
       const { content, latitude, longitude, author } = req.body;
-      console.log(typeof Note);
       const note = new Note({ content, latitude, longitude, author });
+      note_from_db = Note.findOne({ note });
+      if (note_from_db) {
+        return res.status(200).json({ message: 'Note already exists' });
+      }
       await note.save();
-      return res.status(200).json({ message: 'New note created' });
+      return res.status(201).json({ message: 'New note created' });
     } catch (e) {
       return res
         .status(500)
-        .json({ message: 'Some error while creating new note' + e });
+        .json({ message: 'Some error while creating new note ' + e });
     }
   }
 }
