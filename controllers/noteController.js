46f69f3dcb34735c609aa6ebf19ffea04ecaf971
Georgii Voyakin <voyaking@gmail.com>
2022-06-30 16:32:16 +0300

    Change create method

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,15 +1,40 @@
 const Note = require('../models/Note');
 const User = require('../models/User');
- 
+const jwt = require('jsonwebtoken');
+
 class noteController {
   async create(req, res) {
     try {
-      const { content, latitude, longitude, author } = req.body;
-      const note = new Note({ content, latitude, longitude, author });
-      const note_from_db = Note.findOne({ note });
+      const required_fields = ['content', 'latitude', 'longitude'];
+      const isBadRequest = required_fields.some((item) => {
+        if (!Object.hasOwn(req.body, item)) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (isBadRequest) {
+        return res.status(400).json({
+          message: `Some of the required fields: ${required_fields} are missing`,
+        });
+      }
+
+      const token = req.headers.authorization.split(' ')[1];
+      const user_id = jwt.verify(token, process.env.jwtSecretKey).id;
+
+      const { content, latitude, longitude } = req.body;
+      const note_data = {
+        content,
+        latitude,
+        longitude,
+        author: user_id,
+      };
+      const note_from_db = await Note.findOne(note_data).exec();
       if (note_from_db) {
         return res.status(200).json({ message: 'Note already exists' });
       }
+      const note = new Note(note_data);
       await note.save();
       return res.status(201).json({ message: 'New note created' });
     } catch (e) {
@@ -28,23 +53,25 @@ class noteController {
     try {
       const id = req.params.id;
       const { content, latitude, longitude, author } = req.body;
-        const note_from_db = Note.findById(id);
+      const note_from_db = Note.findById(id);
       if (note_from_db !== undefined) {
-          console.log(content);
-          console.log(latitude);
-          console.log(longitude);
-          console.log(author);
-        if (author!== undefined) {
-            console.log(User.findById(author));
-            if (User.findById(author) === undefined) {
-        return res.status(400).json({ message: 'Trying to update note for not existing user' });
-            }
+        console.log(content);
+        console.log(latitude);
+        console.log(longitude);
+        console.log(author);
+        if (author !== undefined) {
+          console.log(User.findById(author));
+          if (User.findById(author) === undefined) {
+            return res
+              .status(400)
+              .json({ message: 'Trying to update note for not existing user' });
+          }
         }
         if (content !== undefined) {
         }
         if (latitude !== undefined) {
         }
-        if (longitude!== undefined) {
+        if (longitude !== undefined) {
         }
         return res.status(200).json({ message: 'Note updated' });
       }
